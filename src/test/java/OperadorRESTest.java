f5948dafd961b3f805703b12b83dcfc86e7d8bc1
Ariel <c.ariel@gmail.com>
2017-06-06 20:51:43 -0300

    Agrego test compuestos para suma y resta

--- src/test/java/OperadorRESTest.java
+++ src/test/java/OperadorRESTest.java
@@ -10,9 +10,11 @@ public class OperadorRESTest {
 	public void test() throws IndicadorException {
 		
 	
-		Factor numeroA = new Numero(2);
-		Factor numeroB = new Numero(3);
+		Factor numeroA = new Numero(10);
+		Factor numeroB = new Numero(4);
+		Factor numeroC = new Numero(6);
 		OperadorTermino resta = new OperadorRES();
+		OperadorTermino resta1 = new OperadorRES();
 		
 		Termino termino1 = new Termino();
 		termino1.addFactor(numeroA);
@@ -20,11 +22,21 @@ public class OperadorRESTest {
 		Termino termino2 = new Termino();
 		termino2.addFactor(numeroB);
 		
-		/*Indicador indicador = new Indicador();
+		Termino termino3 = new Termino();
+		termino3.addFactor(numeroC);
+		
+		//testeo 10 - 4 = 6
+		assertEquals(6, resta.operar(termino1.getResultado(), termino2), 0.01);
+		
+		Indicador indicador = new Indicador();
 		indicador.addTermino(termino1);
 		indicador.addTermino(termino2);
-		indicador.addOperador(suma);*/
-		assertEquals(5, resta.operar(termino1.getResultado(), termino2), 0.01);
+		indicador.addTermino(termino3);
+		indicador.addOperador(resta);
+		indicador.addOperador(resta1);
+		
+		assertEquals(0, indicador.getResultado(), 0.01);
+		
 		
 		
 	}
--- src/test/java/OperadorSUMTest.java
+++ src/test/java/OperadorSUMTest.java
@@ -12,7 +12,9 @@ public class OperadorSUMTest {
 	
 		Factor numeroA = new Numero(2);
 		Factor numeroB = new Numero(3);
+		Factor numeroC = new Numero(4);
 		OperadorTermino suma = new OperadorSUM();
+		OperadorTermino suma1 = new OperadorSUM();
 		
 		Termino termino1 = new Termino();
 		termino1.addFactor(numeroA);
@@ -20,11 +22,22 @@ public class OperadorSUMTest {
 		Termino termino2 = new Termino();
 		termino2.addFactor(numeroB);
 		
-		/*Indicador indicador = new Indicador();
+		// testeo 2+3  = 5 
+		assertEquals(5, suma.operar(termino1.getResultado(), termino2), 0.01);
+		
+		
+		
+		Termino termino3 = new Termino();
+		termino3.addFactor(numeroC);
+		
+		// testeo 2+3+4 = 9
+		Indicador indicador = new Indicador();
 		indicador.addTermino(termino1);
 		indicador.addTermino(termino2);
-		indicador.addOperador(suma);*/
-		assertEquals(5, suma.operar(termino1.getResultado(), termino2), 0.01);
+		indicador.addTermino(termino3);
+		indicador.addOperador(suma);
+		indicador.addOperador(suma1);
+		assertEquals(9, indicador.getResultado(), 0.01);
 		
 		
 	}
