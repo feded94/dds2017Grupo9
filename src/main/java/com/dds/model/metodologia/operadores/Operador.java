e1758a80ca9d73524480645c02ff10ea328d97ad
wzfc <wzfernandochen@gmail.com>
2017-10-25 02:21:30 -0300

    Clase OperadorFactory
    No haria falta guardar los operadores en la BD

--- src/main/java/com/dds/model/metodologia/operadores/Operador.java
+++ src/main/java/com/dds/model/metodologia/operadores/Operador.java
@@ -2,4 +2,5 @@ package com.dds.model.metodologia.operadores;
 
 public interface Operador {
     String nombre();
+    // TODO: String descripcion();
 }
---
+++ src/main/java/com/dds/model/metodologia/operadores/OperadorFactory.java
@@ -0,0 +1,45 @@
+package com.dds.model.metodologia.operadores;
+
+import com.dds.model.metodologia.operadores.binarios.*;
+import com.dds.model.metodologia.operadores.unarios.Creciente;
+import com.dds.model.metodologia.operadores.unarios.CrecienteEstricto;
+import com.dds.model.metodologia.operadores.unarios.Decreciente;
+import com.dds.model.metodologia.operadores.unarios.DecrecienteEstricto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class OperadorFactory {
+    private static final List<Operador> operadores;
+
+    static {
+        operadores = new ArrayList<>(10);
+
+        operadores.add(new Igual());
+        operadores.add(new Distinto());
+        operadores.add(new Mayor());
+        operadores.add(new MayorOIgual());
+        operadores.add(new Menor());
+        operadores.add(new MenorOIgual());
+
+        operadores.add(new Creciente());
+        operadores.add(new CrecienteEstricto());
+        operadores.add(new Decreciente());
+        operadores.add(new DecrecienteEstricto());
+    }
+
+    private OperadorFactory() {}
+
+    public static List<Operador> getOperadores() {
+        return operadores;
+    }
+
+    public static Operador getOperador(String nombreOperador) {
+        for (Operador op : operadores) {
+            if (op.nombre().equals(nombreOperador))
+                return op;
+        }
+
+        return null;
+    }
+}
